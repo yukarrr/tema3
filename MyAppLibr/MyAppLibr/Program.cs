4adefdc495079021b30584f1bf65dc3df532d6f4
yukarrr <yukarrr8@gmail.com>
2024-11-22 20:53:18 +0900

    Initial commit

---
+++ MyAppLibr/MyAppLibr/Program.cs
@@ -0,0 +1,34 @@
+﻿using MyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAppLibr
+{
+    internal class Program
+    {
+        class Program1
+        {
+            static void Main(string[] args)
+            {
+                MyLibrary myLib = new MyLibrary();
+
+                myLib.Hello();
+                myLib.User("Юля");
+
+                int result = myLib.sum(5, 3);
+                Console.WriteLine($"Сумма: {result}");
+
+                int dellPrint = myLib.dell(10, 2);
+                Console.WriteLine(dellPrint);
+
+                bool isEven = myLib.IsEven(4);
+                Console.WriteLine($"Число четное? {isEven}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
